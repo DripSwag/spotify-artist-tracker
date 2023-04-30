c5ce8daae1ef9db22793c4677525a66ff0f049ed
DripSwag <mimirani33@gmail.com>
2023-04-30 10:57:25 +0200

    Added access code getter function

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,12 +1,30 @@
 function HomePage(){
+
 	const getSpotifyCode = () => {
 		const url = window.location.href
 		const splittedUrl = url.split("=")
 		return splittedUrl[1]
 	}
 
+	const getAccessCode = async () => {
+		const spotifyAccessCode = await fetch('http://127.0.0.1:8000/api/spotifyAccessCodeUpdate/', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				userId: 1,
+				code: getSpotifyCode()
+			})
+		})
+
+		return spotifyAccessCode
+	}
+
 	return(
-		<h1>Homepage</h1>
+		<div>
+			<h1>Homepage</h1>
+		</div>
 	)
 }
 export default HomePage
