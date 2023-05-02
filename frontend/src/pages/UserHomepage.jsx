3aa5a05c4a9da64e58e634750f3393e1193755ad
DripSwag <mimirani33@gmail.com>
2023-05-02 18:55:47 +0200

    "Added getArtistIds function"

--- frontend/src/pages/UserHomepage.jsx
+++ frontend/src/pages/UserHomepage.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react"
 function UserHomepage(){
 	const location = useLocation()
 	const [accessCode, setAccessCode] = useState()
+	const [artists, setArtists] = useState()
 
 	const getAccessCode = async () => {
 		const spotifyAccessCode = await fetch('http://127.0.0.1:8000/api/spotifyAccessCodeUpdate/', {
@@ -20,6 +21,12 @@ function UserHomepage(){
 		spotifyAccessCode.status === 201 ? setAccessCode(true) : setAccessCode(false)
 	}
 
+	//Make this a useCallback. When i make a search input field, this function should be called
+	const getAritstIds = async () => {
+		const artistIds = await fetch(`http://127.0.0.1:8000/api/artistIdsGet/${location.state.id}`).then((data) => data.json())
+		setArtists(artistIds)
+	}
+
 	useEffect(() => {
 		getAccessCode()
 	}, [getAritstIds])
@@ -27,6 +34,7 @@ function UserHomepage(){
 	return(
 		<section>
 			<button onClick={() => {console.log(accessCode)}}>accessCode</button>
+			<button onClick={() => {console.log(artists)}}>aritsts</button>
 		</section>
 	)
 }
