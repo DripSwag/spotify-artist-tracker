675ecbff4c6bc0ed79b149f494a30ebf59d0bd84
DripSwag <mimirani33@gmail.com>
2023-05-07 21:19:01 +0200

    "Added queue header into artistShowcase"

--- frontend/src/components/ArtistShowcase.jsx
+++ frontend/src/components/ArtistShowcase.jsx
@@ -3,6 +3,7 @@ import ArtistLabel from "./ArtistLabel"
 function ArtistShowcase({ artists, getArtistsIds, searching }){
 	return(
 		<div className={`h-full w-full overflow-auto flex flex-col gap-4 bg-neutral-900 rounded-lg p-4 ${searching ? 'hidden' : ''}`}>
+			<h1 className="font-bold text-4xl my-2">Queue</h1>
 			{
 				artists && artists.map((data, index) => {
 					return <ArtistLabel key={data['id']} queueNumber={index + 1} pk={data['id']} name={data['name']} pictureLink={data['pictureLink']} getArtistsIds={getArtistsIds}/>
