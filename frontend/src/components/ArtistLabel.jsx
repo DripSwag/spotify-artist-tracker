592c3ab366b48b9ea4aba1164c27a4b35a14ad87
DripSwag <mimirani33@gmail.com>
2023-05-07 09:08:36 +0100

    "Added deleting artists from the queue"

--- frontend/src/components/ArtistLabel.jsx
+++ frontend/src/components/ArtistLabel.jsx
@@ -1,9 +1,17 @@
-function ArtistLabel({ artistId, pk, name, pictureLink, queueNumber }){
+function ArtistLabel({ pk, name, pictureLink, queueNumber }){
+
+	const removeFromQueue = async () => {
+		const response = await fetch(`http://127.0.0.1:8000/api/artistIdDelete/${pk}`,{
+			method: 'DELETE'
+		})
+	}
+
 	return(
-		<div className="flex items-center h-12 gap-6 rounded-lg">
+		<div className="flex items-center h-12 gap-6 rounded-lg relative">
 			<p>{queueNumber}</p>
 			<img src={pictureLink} className="h-full aspect-square" ></img>
 			<p className="text-md font-medium">{name}</p>
+			<button onClick={removeFromQueue} className="text-2xl font-extrabold text-neutral-600 cursor-default absolute right-2 hover:text-white">x</button>
 		</div>
 	)
 }
--- frontend/src/components/ArtistShowcase.jsx
+++ frontend/src/components/ArtistShowcase.jsx
@@ -17,7 +17,7 @@ function ArtistShowcase(){
 		<div className="h-full overflow-auto flex flex-col gap-4 bg-neutral-900 m-2 rounded-lg p-4">
 			{
 				artists && artists.map((data, index) => {
-					return <ArtistLabel key={data['id']} queueNumber={index} artistId={data['artistId']} pk={data['id']} name={data['name']} pictureLink={data['pictureLink']} />
+					return <ArtistLabel key={data['id']} queueNumber={index + 1} pk={data['id']} name={data['name']} pictureLink={data['pictureLink']} />
 			})
 			}
 		</div>
