a6e8b3cfd6312c8833d112f4a07c5274efce9eea
DripSwag <mimirani33@gmail.com>
2023-05-07 21:43:48 +0200

    "Added delay when searching before get call is sent"

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,29 +1,36 @@
-import { useRef, useState } from "react"
+import { useRef, useState, useEffect } from "react"
 import AritstSearchResult from "./ArtistSearchResult"
 
 function SearchBar({ userId, getArtistsIds, setSearching, searching }){
 	const searchBarRef = useRef(null)
+	const [searchBody, setSearchBody] = useState()
 	const [searchResults, setSearchResults] = useState([])
-
-	const searchArtist = async (searchQuery) => {
-		if(searchQuery){
-			const artistSearchBody = await fetch(`http://127.0.0.1:8000/api/searchArtists/${userId}/${parseSearchQuery(searchQuery)}`).then((data) => data.json())
-			setSearchResults(artistSearchBody)
-			setSearching(true)
-		}
-		else{
-			setSearchResults([])
-			setSearching(false)
-		}
-	}
 
 	const parseSearchQuery = (searchQuery) => {
 		return searchQuery.replace(' ', '%20')
 	}
 
+	useEffect(() => {
+		const timeOut = setTimeout(async () => {
+			if(searchBody){
+				const artistSearchBody = await fetch(`http://127.0.0.1:8000/api/searchArtists/${userId}/${parseSearchQuery(searchBody)}`).then((data) => data.json())
+				setSearchResults(artistSearchBody)
+				setSearching(true)
+				console.log("search")
+			}
+			else{
+				setSearchResults([])
+				setSearching(false)
+				console.log("not search")
+			}
+
+			return () => clearTimeout(timeOut)
+		}, 400)
+	}, [searchBody])
+
 	return(
 		<div className={`w-full relative flex flex-col ${searching ? 'h-full' : ''}`}>
-			<input type='search' ref={searchBarRef} placeholder='Search Artist' onChange={(input) => {searchArtist(input.target.value)}} className='p-2 rounded-lg font-bold relative left-1/2 -translate-x-1/2'></input>
+			<input type='search' ref={searchBarRef} placeholder='Search Artist' onChange={(input) => {setSearchBody(input.target.value)}} className='p-2 rounded-lg font-bold relative left-1/2 -translate-x-1/2'></input>
 			<div className={`overflow-y-auto flex flex-col gap-8 h-full py-8 ${searching ? '' : 'hidden'}`}>
 				<h1 className="font-bold text-4xl">Artists</h1>
 				{
