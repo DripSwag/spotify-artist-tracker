87f0dfd62da60511674b984bf551ac9f9b82460d
DripSwag <mimirani33@gmail.com>
2023-05-08 10:50:26 +0200

    "When searching, added delay between showing artists header and the actual results"

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -16,11 +16,9 @@ function SearchBar({ userId, getArtistsIds, setSearching, searching }){
 			if(searchBody){
 				const artistSearchBody = await fetch(`http://127.0.0.1:8000/api/searchArtists/${userId}/${parseSearchQuery(searchBody)}`).then((data) => data.json())
 				setSearchResults(artistSearchBody)
-				setSearching(true)
 			}
 			else{
 				setSearchResults([])
-				setSearching(false)
 			}
 
 			return () => clearTimeout(timeOut)
@@ -29,7 +27,7 @@ function SearchBar({ userId, getArtistsIds, setSearching, searching }){
 
 	return(
 		<div className={`w-full relative flex flex-col ${searching ? 'h-full' : ''}`}>
-			<input type='search' ref={searchBarRef} placeholder='Search Artist' onChange={(input) => {setSearchBody(input.target.value)}} className='p-2 rounded-lg font-bold relative left-1/2 -translate-x-1/2'></input>
+			<input type='search' ref={searchBarRef} placeholder='Search Artist' onChange={(input) => {setSearchBody(input.target.value); input.target.value ? setSearching(true) : setSearching(false)}} className='p-2 rounded-lg font-bold relative left-1/2 -translate-x-1/2'></input>
 			<div className={`overflow-y-auto flex flex-col gap-8 h-full py-8 ${searching ? '' : 'hidden'}`}>
 				<h1 className="font-bold text-4xl">Artists</h1>
 				{
